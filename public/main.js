e2ea6d47d3610c42a10a613bb47826db5678df96
CarlosAllberto <dasilvacarlosalberto344@gmail.com>
2024-05-08 22:05:56 -0300

    entrando no chat

--- public/main.js
+++ public/main.js
@@ -1,1 +1,21 @@
 const socket = io()
+
+let userList = []
+
+const loginPage = document.querySelector('#login-page')
+const chatPage = document.querySelector('#chat-page')
+const loginInput = document.querySelector('#login-input')
+chatPage.style.display = 'none'
+var userName = ''
+
+loginInput.addEventListener('keyup', e => {
+  if (e.keyCode === 13) {
+    let name = loginInput.value.trim()
+    if (name != '') {
+      userName = loginInput.value 
+      document.title = userName
+      loginPage.style.display = 'none'
+      chatPage.style.display = 'inline'
+    }
+  }
+})
