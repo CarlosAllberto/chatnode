609b5988acd6cbb63670e2b017c2ebeb687d5610
CarlosAllberto <dasilvacarlosalberto344@gmail.com>
2024-05-18 16:13:46 -0300

    listando usuários

--- public/main.js
+++ public/main.js
@@ -5,17 +5,40 @@ let userList = []
 const loginPage = document.querySelector('#login-page')
 const chatPage = document.querySelector('#chat-page')
 const loginInput = document.querySelector('#login-input')
+const textInput = document.querySelector('#text-input')
 chatPage.style.display = 'none'
-var userName = ''
+var username = ''
+
+loginInput.focus()
+
+const renderUserList = () => {
+  let ul = document.querySelector('.user-list')
+  ul.innerHTML = ''
+  userList.forEach(e => {
+    ul.innerHTML += `<li>${e}</li>`
+  })
+}
 
 loginInput.addEventListener('keyup', e => {
   if (e.keyCode === 13) {
     let name = loginInput.value.trim()
     if (name != '') {
-      userName = loginInput.value 
-      document.title = userName
-      loginPage.style.display = 'none'
-      chatPage.style.display = 'inline'
+      username = loginInput.value 
+      document.title = `${username} | Chat`
+      socket.emit('join', username)
     }
   }
+})
+
+socket.on('user-ok', list => {
+  loginPage.style.display = 'none'
+  chatPage.style.display = 'inline'
+  textInput.focus()
+  userList = list
+  renderUserList()
+})
+
+socket.on('list-update', data => {
+  userList = data.list
+  renderUserList()
 })
--- src/server.js
+++ src/server.js
@@ -5,4 +5,27 @@ app.set('port', 7777)
 const server = app.listen(app.get('port'), () => console.log('Servidor iniciado'))
 const io = socketIo(server)
 
-io.on('connection', socket => console.log('usuário conectado'))
+let connectedUsers = []
+
+io.on('connection', socket => {
+  console.log('usuário conectado')
+  socket.on('join', username => {
+    socket.username = username
+    connectedUsers.push(username)
+    console.log(connectedUsers)
+
+    socket.emit('user-ok', connectedUsers)
+    socket.broadcast.emit('list-update', {
+      joined: username,
+      list: connectedUsers
+    })
+  })
+
+  socket.on('disconnect', () => {
+    connectedUsers = connectedUsers.filter(e => e != socket.username)
+    socket.broadcast.emit('list-update', {
+      left: socket.username,
+      list: connectedUsers
+    })
+  })
+})
